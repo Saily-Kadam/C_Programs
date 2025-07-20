fd826116ac2371b77ff5852c509168ff346308cf
Saily-Kadam <kadamsaily761@gmail.com>
2025-07-20 12:55:12 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -4,7 +4,7 @@ int main()
     int i=0, j=0;
     printf("Enter two numbers:");
     scanf("%d %d", &i, &j);
-    printf(i="%d" , j="%d", i, j);
-    printf("The sum of the two given numbers is:",i+j);
+    printf("i=%d , j=%d", i, j);
+    printf("The sum of the two given numbers is:%d\n",i+j);
     return 0;
 }
