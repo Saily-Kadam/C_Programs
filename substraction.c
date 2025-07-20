969b23e3a891fecd3de479a888719bb5ef5ce7b9
Saily-Kadam <kadamsaily761@gmail.com>
2025-07-20 11:29:16 +0530

    Add files via upload

---
+++ substraction.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i =0, j=0;
+    printf("Enter two numbers:");
+    scanf("%d" "%d", &i, &j);
+    printf(i="%d" ,j="%d", i,j);
+    printf("The difference is:", i-j);
+    return 0;
+
+}
