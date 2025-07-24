9896d8bdd21ece59607b2fcfe25da0684edc3f3e
Saily-Kadam <kadamsaily761@gmail.com>
2025-07-24 09:23:30 +0530

    Add files via upload

---
+++ sizeofdatatypes.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("Size of integer %d\n",sizeof(int));
+    printf("Size of character %d\n",sizeof(char));
+    printf("Size of float %d\n",sizeof(float));
+    printf("Size of double %d\n",sizeof(double));
+    return 0;
+}
