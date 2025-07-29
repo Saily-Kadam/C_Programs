c438f5b83ac38e91399aae9b72212c055e11c6e7
Saily-Kadam <kadamsaily761@gmail.com>
2025-07-29 13:03:32 +0530

    Add files via upload

---
+++ perimeter.c
@@ -0,0 +1,12 @@
+// Perimeter of circle
+#include<stdio.h>
+int main()
+{
+    int r = 0;
+    printf("Enter the radius of circle");
+    scanf("%d",&r);
+    int perimeter = 2*3.14*r;
+    printf("The perimeter of circle is %d",perimeter);
+    return 0;
+    
+}
