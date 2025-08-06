8b0d90d82951088b01dc3f0ecb9dca3a82babf25
Saily-Kadam <kadamsaily761@gmail.com>
2025-08-06 08:35:44 +0530

    Add files via upload

---
+++ xsquare.c
@@ -0,0 +1,10 @@
+//Accept an integer  and compute x^2
+#include<stdio.h>
+int main()
+{
+    int x=0;
+    printf("Enter an integer");
+    scanf("%d", &x);
+    printf("x^2 = %d", x*x);
+    return 0;
+}
