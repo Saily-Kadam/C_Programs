01f5a37266995cc417385ff3b58b55497dc11928
Saily-Kadam <kadamsaily761@gmail.com>
2025-08-06 08:36:02 +0530

    Add files via upload

---
+++ xsquareplustwox.c
@@ -0,0 +1,10 @@
+//compute x^2 +2x
+#include<stdio.h>
+int main()
+{
+    int x =0;
+    printf("Give the value x");
+    scanf("%d", &x);
+    printf("The value of x^2+2x is %d", x*x+2*x);
+    return 0;
+}
