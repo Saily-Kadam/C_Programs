a9da4cb582c99010b898c52fe288672b92ad96de
Saily-Kadam <kadamsaily761@gmail.com>
2025-08-10 12:13:54 +0530

    Add files via upload

---
+++ alphabets.c
@@ -0,0 +1,19 @@
+// Check if a character entered by the user is an alphabet or not.
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        printf("'%c' is an alphabet.\n", ch);
+    } else {
+        printf("'%c' is not an alphabet.\n", ch);
+    }
+
+    return 0;
+}
