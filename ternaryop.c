2c6bb1b86517f3a98b023b1a3a4666eae662f31d
Saily-Kadam <kadamsaily761@gmail.com>
2025-08-16 08:40:42 +0530

    Add files via upload

---
+++ ternaryop.c
@@ -0,0 +1,15 @@
+// Use a ternary operator to find if a number is even or odd.
+#include <stdio.h>
+
+int main() {
+    int num;
+
+   
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    
+    (num % 2 == 0) ? printf("Even\n") : printf("Odd\n");
+
+    return 0;
+}
