b3435dfa4f7299f3b09239a8d467364149d0293c
Saily-Kadam <kadamsaily761@gmail.com>
2025-08-23 23:03:17 +0530

    Add files via upload

---
+++ count.c
@@ -0,0 +1,27 @@
+//Count and display the number of digits in a number 
+#include <stdio.h>
+
+int main() {
+    int number, count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    
+    if (number == 0) {
+        count = 1;
+    } else {
+       
+        if (number < 0)
+            number = -number;
+
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+    }
+
+    printf("Number of digits: %d\n", count);
+
+    return 0;
+}
