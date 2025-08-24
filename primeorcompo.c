0b0056798022eee169e04600ff206c2295e98b25
Saily-Kadam <kadamsaily761@gmail.com>
2025-08-24 18:51:11 +0530

    Add files via upload

---
+++ primeorcompo.c
@@ -0,0 +1,28 @@
+// Check whether a given number is prime or composite  
+#include <stdio.h>
+
+int main() {
+    int num, i, isPrime = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        printf("%d is neither prime nor composite.\n", num);
+        return 0;
+    }
+
+    for (i = 2; i <= num / 2; ++i) {
+        if (num % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is a composite number.\n", num);
+
+    return 0;
+}
