e8a1d47ddcf137aae58b66ebd5462b8cea458e21
Saily-Kadam <kadamsaily761@gmail.com>
2025-09-07 10:12:15 +0530

    Add files via upload

---
+++ 1nestedloop.c
@@ -0,0 +1,18 @@
+//Print multiplication tables from 2 to 11 using nested for loops.
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 2; i <= 11; i++) {
+        printf("Multiplication Table for %d:\n", i);
+       
+        for (j = 1; j <= 10; j++) {
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
