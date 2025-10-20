c76898541421fb501977f420acd0e8dcfaf76cbf
Saily-Kadam <kadamsaily761@gmail.com>
2025-10-21 00:15:32 +0530

    Add files via upload

--- month.c
+++ month.c
@@ -1,47 +1,28 @@
-#include<stdio.h>
-int main()
-{
-    int month =0;
-    printf("Enter month");
-    scanf("%d",&month);
-
-    switch(month) {
-    case 1:
-        printf("January");
-        break;
-    case 2:
-        printf("February");
-        break;
-    case 3:
-        printf("March");
-        break;
-    case 4:
-        printf("April");
-        break;
-    case 5:
-        printf("May");
-        break;
-    case 6:
-        printf("June");
-        break;
-    case 7:
-        printf("July");
-        break;
-    case 8:
-        printf("August");
-        break;
-    case 9:
-        printf("September");
-        break;
-    case 10:
-        printf("Octomber");
-        break;
-    case 11:
-        printf("November");
-        break;
-    case 12:
-        printf("December");
-        break;                                              
-}
-   return 0;
-}
+/* Input month number and display the number of days in the month (consider leap years
+ for February).*/
+ #include<stdio.h>
+ int main()
+ {
+    int i=0, year=0;
+    printf("Enter a number\n");
+    scanf("%d", &i);
+    if(i==2){
+        printf("Enter year:");
+        scanf("%d", &year);
+        if((year%4==0 && year%100!=0) || year%400==0){
+            printf("Number of days are 29");
+        }
+        else{
+            printf("Number of days are 28");
+        }
+    }
+    else if(i==1 || i==3 || i==5 || i==7 ||i==8||i==10||i==12){
+        printf("The number of days are 31\n");
+    }
+    else if(i==4 || i== 6||i==9||i==11){
+        printf("The number of days are 30\n");
+    }
+    else {
+        printf("Invalid input\n");
+    }
+ }
