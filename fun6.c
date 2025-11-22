3d47546c509aa85f47d41466178b402d86269802
Saily-Kadam <kadamsaily761@gmail.com>
2025-11-22 19:26:04 +0530

    Add files via upload

---
+++ fun6.c
@@ -0,0 +1,16 @@
+//displaying a message
+#include <stdio.h>
+
+// Function declaration
+void displayMessage();
+
+int main() {
+    // Function call
+    displayMessage();
+    return 0;
+}
+
+// Function definition
+void displayMessage() {
+    printf("Hello! This message is displayed using a function.\n");
+}
