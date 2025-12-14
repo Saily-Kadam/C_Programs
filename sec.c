528a8026b25253710198bff7252e2591c4b3e089
Saily-Kadam <kadamsaily761@gmail.com>
2025-12-14 14:41:22 +0530

    Add files via upload

---
+++ sec.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a[50], n;
+    int i, largest, second;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    largest = second = a[0];
+
+    for (i = 1; i < n; i++) {
+        if (a[i] > largest) {
+            second = largest;
+            largest = a[i];
+        } else if (a[i] > second && a[i] != largest) {
+            second = a[i];
+        }
+    }
+
+    printf("Second Largest Element = %d\n", second);
+    return 0;
+}
