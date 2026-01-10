76d4eda3d512f0f4360ffcbc2e05f746939aa8f3
Saily-Kadam <kadamsaily761@gmail.com>
2026-01-10 16:05:03 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+float average(int arr[], int n);
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Average = %.2f\n", average(arr, n));
+
+    return 0;
+}
+
+float average(int arr[], int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+        sum += arr[i];
+    return (float)sum / n;
+}
