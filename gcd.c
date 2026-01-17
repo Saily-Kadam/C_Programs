5f0b95628c936a4be4f1ed27273e824a3eca8361
Saily-Kadam <kadamsaily761@gmail.com>
2026-01-17 17:55:31 +0530

    Add files via upload

--- gcd.c
+++ gcd.c
@@ -1,28 +1,18 @@
-// Find the GCD (HCF) and LCM of two numbers
 #include <stdio.h>
 
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
 int main() {
-    int num1, num2, a, b, gcd, lcm;
+    int a, b;
 
-    // Input two positive integers
-    printf("Enter two positive integers: ");
-    scanf("%d %d", &num1, &num2);
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
 
-    a = num1;
-    b = num2;
-
-    // Find GCD using Euclidean algorithm with while loop
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-
-    gcd = a;
-    lcm = (num1 * num2) / gcd;
-
-    printf("GCD (HCF) of %d and %d is: %d\n", num1, num2, gcd);
-    printf("LCM of %d and %d is: %d\n", num1, num2, lcm);
+    printf("GCD = %d\n", gcd(a, b));
 
     return 0;
 }
