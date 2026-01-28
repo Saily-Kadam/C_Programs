b875963635c0107c668f5cb82ada1d6d8e5a1cfc
Saily-Kadam <kadamsaily761@gmail.com>
2026-01-28 18:36:06 +0530

    Add files via upload

---
+++ cntposneg.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int arr[100];
+    int pos = 0, neg = 0, zero = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] > 0)
+            pos++;
+        else if(arr[i] < 0)
+            neg++;
+        else
+            zero++;
+    }
+
+    printf("Positive numbers = %d\n", pos);
+    printf("Negative numbers = %d\n", neg);
+    printf("Zeros = %d\n", zero);
+
+    return 0;
+}
