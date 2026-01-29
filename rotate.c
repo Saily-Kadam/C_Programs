d12c0739a91ecd696d8e8116d0862c79f6d541cf
Saily-Kadam <kadamsaily761@gmail.com>
2026-01-29 13:17:23 +0530

    Add files via upload

--- rotate.c
+++ rotate.c
@@ -1,31 +1,30 @@
-//rotate the array by k steps
 #include <stdio.h>
 
 int main() {
-    int arr[100], rotated[100];
-    int n, k, i;
+    int n;
+    int arr[100];
+    int temp;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    printf("Enter %d elements:\n", n);
-    for(i = 0; i < n; i++) {
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    printf("Enter number of rotations (k): ");
-    scanf("%d", &k);
-    k = k % n;
+    temp = arr[0];
 
-    for(i = 0; i < n; i++) {
-        rotated[(i + k) % n] = arr[i];
+    for(int i = 0; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
     }
 
-    printf("Array after rotating to the right by %d steps:\n", k);
-    for(i = 0; i < n; i++) {
-        printf("%d ", rotated[i]);
+    arr[n - 1] = temp;
+
+    printf("Array after left rotation:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
-    printf("\n");
 
     return 0;
 }
